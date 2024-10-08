a99015e16615d43ab833f00f5cba74e1df5166d7
Martin4331 <sorasdekkan12@gmail.com>
2024-10-09 01:10:10 +0200

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Oct  9 01:09:10 2024
+
+@author: soras
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Aug 27 17:19:28 2024
 
 @author: soras
@@ -7,5 +14,8 @@ Created on Tue Aug 27 17:19:28 2024
 
 from turtle import *
 
-a
+for f in range(4):
+    forward(100)
+    left(360/4)
+   
 turtle.done()
